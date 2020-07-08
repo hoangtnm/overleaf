0ec10529778f0c0e0eccd8951bcb57c980028368
hoangtnm <minhhoangtrannhat@gmail.com>
2020-07-08 19:47:16 +0700

    refactor: install_fonts.sh

--- install_fonts.sh
+++ install_fonts.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
+set -e
+
 font_dir=/usr/share/fonts
 
 # Roboto Mono
 wget 'https://fonts.google.com/download?family=Roboto%20Mono' -O Roboto_Mono.zip && \
-unzip Roboto_Mono.zip -d $font_dir/roboto-mono && \
+unzip Roboto_Mono.zip -d "${font_dir}/roboto-mono"
 rm Roboto_Mono.zip
 
 # Source Serif Pro, Source Sans Pro and Source Code Pro
@@ -12,11 +14,10 @@ git clone --depth 1 https://github.com/adobe-fonts/source-serif-pro.git
 git clone --depth 1 https://github.com/adobe-fonts/source-sans-pro.git
 git clone --depth 1 https://github.com/adobe-fonts/source-code-pro.git
 
-for folder in source*pro
-do
-    # Keep only OTF font files
-    mv $folder/OTF/* $folder 
-    find $folder -mindepth 1 -maxdepth 1 ! -name '*.otf' -exec rm -rf {} \;
+for dir in source*pro; do
+  # Keep only OTF font files
+  mv "${dir}/OTF/"* "${dir}"
+  find "${dir}" -mindepth 1 -maxdepth 1 ! -name '*.otf' -exec rm -rf {} \;
 done
 
 # Rebuild cached list of fonts

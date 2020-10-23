c60a20d51b32d01a0c683bcedd266c2095955f0f
hoangtnm <minhhoangtrannhat@gmail.com>
2020-10-23 21:45:04 +0700

    feat: update install_fonts.sh

--- install_fonts.sh
+++ install_fonts.sh
@@ -5,24 +5,24 @@ set -e
 font_dir=/usr/share/fonts
 
 # Roboto Mono
-wget 'https://fonts.google.com/download?family=Roboto%20Mono' -O Roboto_Mono.zip && \
+wget 'https://fonts.google.com/download?family=Roboto%20Mono' -O Roboto_Mono.zip
 unzip Roboto_Mono.zip -d "${font_dir}/roboto-mono"
 rm Roboto_Mono.zip
 
 # Source Serif Pro, Source Sans Pro and Source Code Pro
-git clone --depth 1 https://github.com/adobe-fonts/source-serif-pro.git
-git clone --depth 1 https://github.com/adobe-fonts/source-sans-pro.git
-git clone --depth 1 https://github.com/adobe-fonts/source-code-pro.git
+git clone -b release --depth 1 https://github.com/adobe-fonts/source-serif-pro.git "${font_dir}/source-serif-pro"
+git clone -b release --depth 1 https://github.com/adobe-fonts/source-sans-pro.git "${font_dir}/source-sans-pro"
+git clone -b release --depth 1 https://github.com/adobe-fonts/source-code-pro.git "${font_dir}/source-code-pro"
 
-for dir in source*pro; do
+for dir in "${font_dir}/source"*"pro"; do
   # Keep only OTF font files
   mv "${dir}/OTF/"* "${dir}"
   find "${dir}" -mindepth 1 -maxdepth 1 ! -name '*.otf' -exec rm -rf {} \;
 
-  if [[ "${dir}" = 'source-sans-pro' ]]; then
+  if [[ "${dir}" = "${font_dir}/source-sans-pro" ]]; then
       for file in "${dir}"/*; do
           new_file=$(echo "${file}" | sed 's/3/Pro/')
-          mv "${file}" "${new_file}" 
+          mv "${file}" "${new_file}"
       done
   fi
 done

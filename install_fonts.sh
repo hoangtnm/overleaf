e01644f049e144ec1a3ddca0bcdbcd5c5724f956
hoangtnm <minhhoangtrannhat@gmail.com>
2020-10-24 16:04:14 +0700

    fix: Source Pro fonts

--- install_fonts.sh
+++ install_fonts.sh
@@ -4,28 +4,24 @@ set -e
 
 font_dir=/usr/share/fonts
 
+
 # Roboto Mono
-wget 'https://fonts.google.com/download?family=Roboto%20Mono' -O Roboto_Mono.zip
-unzip Roboto_Mono.zip -d "${font_dir}/roboto-mono"
-rm Roboto_Mono.zip
+wget -O 'Roboto_Mono.zip' 'https://fonts.google.com/download?family=Roboto%20Mono'
+unzip 'Roboto_Mono.zip' -d "${font_dir}/roboto-mono"
+
 
 # Source Serif Pro, Source Sans Pro and Source Code Pro
-git clone -b release --depth 1 https://github.com/adobe-fonts/source-serif-pro.git "${font_dir}/source-serif-pro"
-git clone -b release --depth 1 https://github.com/adobe-fonts/source-sans-pro.git "${font_dir}/source-sans-pro"
-git clone -b release --depth 1 https://github.com/adobe-fonts/source-code-pro.git "${font_dir}/source-code-pro"
+wget -O 'source-serif-pro.zip' 'https://fonts.google.com/download?family=Source%20Serif%20Pro'
+wget -O 'source-sans-pro.zip' 'https://fonts.google.com/download?family=Source%20Sans%20Pro'
+wget -O 'source-code-pro.zip' 'https://fonts.google.com/download?family=Source%20Code%20Pro'
+unzip 'source-serif-pro.zip' -d "${font_dir}/source-serif-pro"
+unzip 'source-sans-pro.zip' -d "${font_dir}/source-sans-pro"
+unzip 'source-code-pro.zip' -d "${font_dir}/source-code-pro"
 
-for dir in "${font_dir}/source"*"pro"; do
-  # Keep only OTF font files
-  mv "${dir}/OTF/"* "${dir}"
-  find "${dir}" -mindepth 1 -maxdepth 1 ! -name '*.otf' -exec rm -rf {} \;
 
-  if [[ "${dir}" = "${font_dir}/source-sans-pro" ]]; then
-      for file in "${dir}"/*; do
-          new_file=$(echo "${file}" | sed 's/3/Pro/')
-          mv "${file}" "${new_file}"
-      done
-  fi
-done
+# Remove intermediate files
+rm *.zip
+
 
 # Rebuild cached list of fonts
 # See details at
